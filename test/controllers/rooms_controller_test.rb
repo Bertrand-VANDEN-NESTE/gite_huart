f43bb5bbb267eadf71e5821e56014266559115e6
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2024-12-22 19:50:49 +0100

    add controler rooms

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class RoomsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get rooms_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get home_show_url
+    get rooms_show_url
     assert_response :success
   end
 end
