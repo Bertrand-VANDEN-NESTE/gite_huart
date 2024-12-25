9786dc8a5499b5aa591b013e665f028301434f98
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2024-12-26 00:53:03 +0100

    feat: Add SCSS structure and PagesController
    - Updated Gemfile and Gemfile.lock to include necessary dependencies.
    - Removed  and replaced it with  for SCSS support.
    - Added SCSS folder structure:  and .
    - Added images to  for use in the application.
    - Created  for handling static pages.
    - Updated  and  to reflect changes.
    - Added initial views for components and pages.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+# Use Sass to process CSS
+gem "sassc-rails"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.5"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'rooms/show'
+  root to: 'pages#welcome'
+
+  get 'index', to:'rooms#index'
+  get 'show', to:'rooms#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
