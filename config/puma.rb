22367a9bcb3255a57c0c83d7203b7c295d25bcb8
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-01-18 18:15:07 +0100

    Fix Puma environment for production on Scalingo

--- config/puma.rb
+++ config/puma.rb
@@ -3,6 +3,6 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 threads min_threads_count, max_threads_count
 
 port ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 plugin :tmp_restart
