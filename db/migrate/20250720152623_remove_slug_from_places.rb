6f954f13db67cfbf5723d62cf75ee8cb6f9b03ec
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-01-25 16:44:38 +0100

    Fix migration RemoveSlugFromPlaces for production

--- db/migrate/20250720152623_remove_slug_from_places.rb
+++ db/migrate/20250720152623_remove_slug_from_places.rb
@@ -1,4 +1,4 @@
-class RemoveSlugFromPlaces < ActiveRecord::Migration[7.1]
+class RemoveSlugFromPlaces < ActiveRecord::Migration[7.2]
   def up
     if column_exists?(:places, :slug)
       remove_column :places, :slug
