f751b6339fe65bcf1925dd667901e10c8f54e340
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-02-02 00:59:01 +0100

    modification burger

--- app/javascript/controllers/burger_controller.js
+++ app/javascript/controllers/burger_controller.js
@@ -3,13 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
-  toggle() {
+  toggle(event) {
+    event.preventDefault()
+
     this.menuTarget.classList.toggle("active")
     document.body.classList.toggle("menu-open")
-  }
-
-  close() {
-    this.menuTarget.classList.remove("active")
-    document.body.classList.remove("menu-open")
   }
 }
