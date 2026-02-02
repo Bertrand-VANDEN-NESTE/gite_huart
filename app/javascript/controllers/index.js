586920b9f60b284b2ee3cc3bd3c593920c86814f
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-02-02 01:38:42 +0100

    Fix JS Stimulus pour production

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,13 @@
 import { application } from "./application"
 
+// Tes contrôleurs Stimulus
 import BurgerController from "./burger_controller"
-import ZoomBackgroundController from "./zoom_background_controller"
 import LightboxController from "./lightbox_controller"
-import CarouselThreeController from "./carousel_three_controller";
+import ZoomBackgroundController from "./zoom_background_controller"
+import CarouselThreeController from "./carousel_three_controller"
 
+// Enregistrement
 application.register("burger", BurgerController)
-application.register("zoom-background", ZoomBackgroundController)
 application.register("lightbox", LightboxController)
-application.register("carsouel-three",CarouselThreeController)
+application.register("zoom-background", ZoomBackgroundController)
+application.register("carousel-three", CarouselThreeController)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.js", preload: true
