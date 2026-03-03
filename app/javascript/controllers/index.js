4be6a9e7841fa8d1af8d4662d50baddb246dc938
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-03-03 22:10:39 +0100

    fix stimulus autoload with importmap

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,19 +1,4 @@
-import { application } from "./application"
+import { application } from "controllers/application"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-// Tes contrôleurs Stimulus
-import BurgerController from "./burger_controller"
-import LightboxController from "./lightbox_controller"
-import ZoomBackgroundController from "./zoom_background_controller"
-import CarouselThreeController from "./carousel_three_controller"
-import ParallaxController from "./parallax_controller"
-import RevealController from "./reveal_controller"
-import ContactProtectController from "./contact_protect_controller"
-
-// Enregistrement
-application.register("burger", BurgerController)
-application.register("lightbox", LightboxController)
-application.register("zoom-background", ZoomBackgroundController)
-application.register("carousel-three", CarouselThreeController)
-application.register("parallax", ParallaxController)
-application.register("reveal", RevealController)
-application.register("contact-protect", ContactProtectController)
+eagerLoadControllersFrom("controllers", application)
