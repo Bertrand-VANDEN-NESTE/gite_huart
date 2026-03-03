519f870cf33b1e11ca73c9ac2d28d1e6e184e2c0
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-03-03 20:35:02 +0100

    debug production errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
   # Mailer
   # ===============================
   config.action_mailer.perform_caching = false
+  config.consider_all_requests_local = true
 
   # IMPORTANT : nécessaire pour générer les URLs dans les emails
   config.action_mailer.default_url_options = {
