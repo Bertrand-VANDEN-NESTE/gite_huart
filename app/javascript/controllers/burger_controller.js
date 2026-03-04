8f8b338aef312bd409f8cbea46cd93308f67d8dd
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2026-03-04 01:29:05 +0100

    modification de la navbar

--- app/javascript/controllers/burger_controller.js
+++ app/javascript/controllers/burger_controller.js
@@ -1,14 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["menu", "button"]
 
   toggle(event) {
     event.preventDefault()
     this.menuTarget.classList.toggle("active")
+    this.buttonTarget.classList.toggle("open")
   }
 
   close() {
     this.menuTarget.classList.remove("active")
+    this.buttonTarget.classList.remove("open")
   }
 }
